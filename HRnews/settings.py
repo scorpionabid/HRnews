8d59c9d2697ac72298a53d70a114ab52d0da9108
scorpioabid <abid.agayev@student.edu.az>
2024-10-26 22:06:01 +0400

    26si

--- HRnews/settings.py
+++ HRnews/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,8 +26,9 @@ SECRET_KEY = 'django-insecure-#ag@2y7@^dol+*ek*y6_!4^sx7n%1c@cz8c7tjcgn$6f)nin86
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*.ngrok-free.app',
+                 'localhost', 
+                 '127.0.0.1']
 
 # Application definition
 
